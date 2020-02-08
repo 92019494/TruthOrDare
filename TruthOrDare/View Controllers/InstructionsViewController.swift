e663c0fe133b0fcea8e33de211356fff24d14023
92019494 <92019494@mail.computerpower.ac.nz>
2020-02-08 23:46:38 +1300

    Update InstructionsViewController.swift

--- TruthOrDare/View Controllers/InstructionsViewController.swift
+++ TruthOrDare/View Controllers/InstructionsViewController.swift
@@ -9,31 +9,11 @@
 import UIKit
 
 class InstructionsViewController: UIViewController {
-
-    var numberOfPlayers: Int = 0
-    var playerOneName: String = ""
-    var playerTwoName: String = ""
-    var playerThreeName: String = ""
-    var playerFourName: String = ""
-    var playerFiveName: String = ""
-    var playerSixName: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBAction func backToGame(_ sender: Any) {
         dismiss(animated: true, completion: nil)
